235c523c1395a2e51d47ac3ee32b9c041a396574
unknown <enecatalin9393@gmail.com>
2021-02-03 15:59:38 +0200

    basic app setup

---
+++ src/test/java/com/example/simpleNoteApp/SimpleNoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleNoteApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleNoteAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
